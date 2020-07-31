0b1dd215dde2f43662d324d4685ad117a02473b0
Michael Jester <m.jester.93@gmail.com>
2020-07-31 04:15:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Code your solution in this file!
 const returnFirstTwoDrivers = (drivers) => {
-  return drivers.splice(0, 2);
+  return drivers.slice(0, 2);
 }
 
 const returnLastTwoDrivers = (drivers) => {
-  return drivers.splice(-2)
+  return drivers.slice(-2)
 }
